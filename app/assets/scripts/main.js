c00715fba7ee88911a757143ceb8d5a83c9b3cc9
Carlos Lopes Alves <carloslopesalves@gmail.com>
2018-09-17 16:08:10 +0100

    animations in progress
    animations in progress

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -13,21 +13,6 @@ $(document).ready(function() {
   });
 
 
-  //Variables for stripes moving
-  var $evenStripe = $('.internet-statement li:nth-child(even)');
-  var $oddStripe = $('.internet-statement li:nth-child(odd)');
-  var $widthpercentage= $('.internet-statement ul');
-
-  var a,
-      b,
-      c,
-      compensation,
-      second,
-      last,
-      percentage;
-
-  var elHeight = $('.internet-statement').height();
-
 //Change section title on scroll
   $(document).scroll(function() {
 
@@ -42,30 +27,31 @@ $(document).ready(function() {
 
      // calculate where the sections start
      //Homepage
-     var statement = ($('.internet-statement').offset().top) - compensation;
+     var statement = ($('.internet-statement').offset().top) ;
      var projects = ($('.projects').offset().top) - compensation;
      var knowmore = ($('.know-more-about').offset().top) - compensation;
-
+     $('.section-title-underlay span').text('Work');
      // Apply text changes
      if (scrollPos >= projects && scrollPos < knowmore) {
-       $('.section-title-underlay span').text('Work');
+      // $('.section-title-underlay span').text('Work');
+
      }
      else {
-       $('.section-title-underlay span').text('');
+       //$('.section-title-underlay span').text('');
      }
 
-     $('.section-title-underlay .bars-animation').toggle(scrollPos >= statement && scrollPos < projects);
+
+     $('.section-title-underlay .movecontainer').toggle(scrollPos >= projects && scrollPos < knowmore);
+
+     $('.section-title-underlay .bars-container').toggle(scrollPos >= statement && scrollPos < projects);
 
      $('.section-title-underlay img').toggle(scrollPos >= knowmore && scrollPos < footer);
 
 
-     var scroll = $(window).scrollTop();
-     $(".coisas").css({'margin-left': scroll*10});
-
 
   }
 
-  //About
+  //About  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   else if ($('body').is('.body-about')) {
 
 
@@ -96,32 +82,6 @@ $(document).ready(function() {
   }
 
 
-//Stripes animation
-
-  //var compensation = ($(window).height()) / 2;
-   //var second = ($('.second').offset().top) - compensation;
-   //var last = ($('.last').offset().top) - compensation;
-
-   var a = ($('.internet-statement').offset().top) - compensation; // section distance from top
-   var b = $(document).scrollTop();
-   var c = elHeight;
-   var percentageb;
-
-   var percentage = (((b-a)/c)*100);
-   var position=(a+(c/2))-b;
-
-   var elWidth=$('.internet-statement ul').width();
-
-
-   var currentwidth= (($('.internet-statement ul').width())/($(window).width()))*100;
-
-   //if (elWidth>230 && position>=0){
-   percentageb = 100-percentage*1.5;
-    //$widthpercentage.css('width', percentageb + '%');
-
-
-   $evenStripe.css('top', percentage + '%');
-   $oddStripe.css('bottom', percentage + '%');
 
 
 
